3fe9e83d59589d84964654258475f4d59791466c
miltom <mili92@gmail.com>
2014-04-06 13:03:32 +0200

    Revert "revert move project to D"
    This reverts commit c006d548f4f044c67ca694a431065ee235f97898.

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,9 +5,7 @@ include $(CLEAR_VARS)
 # OPENCV_CAMERA_MODULES:=on
 # OPENCV_INSTALL_MODULES:=on
 
-# include $(CVROOT)/jni/OpenCV.mk
-
-include ..\..\..\..\NVPACK\OpenCV-2.4.5-Tegra-sdk-r2\sdk\native\jni\OpenCV.mk
+include $(CVROOT)/jni/OpenCV.mk
 
 LOCAL_MODULE    := sudokuhelper
 LOCAL_LDLIBS +=  -llog -ldl
