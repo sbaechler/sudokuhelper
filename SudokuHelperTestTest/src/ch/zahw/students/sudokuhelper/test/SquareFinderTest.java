cd4634a5ab17a76c85ff9ee6df1c8860c0367488
Simon Bächler <simon@stellanera.com>
2014-04-12 21:38:23 +0200

    add test project

---
+++ SudokuHelperTestTest/src/ch/zahw/students/sudokuhelper/test/SquareFinderTest.java
@@ -0,0 +1,15 @@
+package ch.zahw.students.sudokuhelper.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import ch.zahw.students.sudokuhelper.SquareFinder;
+
+public class SquareFinderTest {
+    @Test
+    public void testReverse()
+    {
+        assertEquals( "", "" );
+       }
+    
+
+}
