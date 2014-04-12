8c3f3740dedf5b054c64ef196920316ee2bdaff8
miltom <mili92@gmail.com>
2014-04-12 20:17:17 +0200

    merging 2

--- src/ch/zahw/students/sudokuhelper/CaptureActivity.java
+++ src/ch/zahw/students/sudokuhelper/CaptureActivity.java
@@ -2,16 +2,14 @@ package ch.zahw.students.sudokuhelper;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
-import org.opencv.core.CvType;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.WindowManager;
@@ -20,12 +18,7 @@ public class CaptureActivity extends Activity implements CvCameraViewListener2 {
     private static final String TAG = "SudokuHelper::CaptureActivity";
     
     private CameraBridgeViewBase mOpenCvCameraView;
-<<<<<<< HEAD
-    private Mat mGray;
-    private Mat mRgba;
-=======
     private SudokuTracker sudokuTracker;
->>>>>>> 1fb755756b8bd50c625a645ff1dafd3d51e048b3
     
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -33,11 +26,7 @@ public class CaptureActivity extends Activity implements CvCameraViewListener2 {
             switch (status) {
                 case LoaderCallbackInterface.SUCCESS:
                 {
-<<<<<<< HEAD
-                    Log.i(TAG, "OpenCV loaded successfully");
-=======
                     Log.v(TAG, "OpenCV loaded successfully");
->>>>>>> 1fb755756b8bd50c625a645ff1dafd3d51e048b3
                     mOpenCvCameraView.enableView();
                 } break;
                 default:
@@ -50,10 +39,6 @@ public class CaptureActivity extends Activity implements CvCameraViewListener2 {
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
-<<<<<<< HEAD
-        Log.i(TAG, "called onCreate");
-=======
->>>>>>> 1fb755756b8bd50c625a645ff1dafd3d51e048b3
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         
@@ -80,32 +65,12 @@ public class CaptureActivity extends Activity implements CvCameraViewListener2 {
     }
 
     public void onDestroy() {
-<<<<<<< HEAD
         super.onDestroy();
-=======
-        super.onDestroy();    
->>>>>>> 1fb755756b8bd50c625a645ff1dafd3d51e048b3
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
     }
     
     public void onCameraViewStarted(int width, int height) {
-<<<<<<< HEAD
-        Log.i(TAG, "Camera view started");
-        mRgba = new Mat(height, width, CvType.CV_8UC4);
-        mGray = new Mat(height, width, CvType.CV_8UC1);
-    }
-
-    public void onCameraViewStopped() {
-        mRgba.release();
-        mGray.release();
-        Log.i(TAG, "Camera view stopped");
-    }
-
-    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-        Imgproc.cvtColor(inputFrame.gray(), mRgba, Imgproc.COLOR_GRAY2RGBA, 4);
-        return mRgba;
-=======
         sudokuTracker = new SudokuTracker(width, height);
     }
 
@@ -116,7 +81,6 @@ public class CaptureActivity extends Activity implements CvCameraViewListener2 {
 
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
         return sudokuTracker.detect(inputFrame.gray());
->>>>>>> 1fb755756b8bd50c625a645ff1dafd3d51e048b3
     }
     
 
