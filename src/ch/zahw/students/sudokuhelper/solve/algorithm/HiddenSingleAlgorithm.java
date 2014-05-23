6b4ae10b02730a25d0938fde38eb662f4e8f7385
miltom <mili92@gmail.com>
2014-05-24 00:52:25 +0200

    merge two commits

--- src/ch/zahw/students/sudokuhelper/solve/algorithm/HiddenSingleAlgorithm.java
+++ src/ch/zahw/students/sudokuhelper/solve/algorithm/HiddenSingleAlgorithm.java
@@ -4,94 +4,91 @@ import android.util.Log;
 import ch.zahw.students.sudokuhelper.solve.Sudoku;
 import ch.zahw.students.sudokuhelper.solve.SudokuField;
 
-public class HiddenSingleAlgorithm extends AAlgorithm  {
-	private static final String TAG = "SudokuHelper::HiddenSingleAlgorithm";
-	private boolean startAgain = false;
+public class HiddenSingleAlgorithm extends AAlgorithm {
+    private static final String TAG = "SudokuHelper::HiddenSingleAlgorithm";
+    private boolean startAgain = false;
 
-	public HiddenSingleAlgorithm(int[][] sudoku) {
-		super(sudoku);
-	}
+    public HiddenSingleAlgorithm(int[][] sudoku) {
+        super(sudoku);
+    }
 
-	public HiddenSingleAlgorithm(Sudoku sudoku) {
-		super(sudoku);
-	}
+    public HiddenSingleAlgorithm(Sudoku sudoku) {
+        super(sudoku);
+    }
 
-	@Override
-	public Sudoku solve() {
-		SudokuField sf;
+    @Override
+    public Sudoku solve() {
+        SudokuField sf;
 
-		start:
+        start:
 
-		// über zeilen iterieren
-		for (int row = 0; row < 9; row++) {
+        // über zeilen iterieren
+        for (int row = 0; row < 9; row++) {
 
-			// über spalten iterieren
-			for (int column = 0; column < 9; column++) {
-				// sudokuField
-				sf = sudoku.getField(row, column);
+            // über spalten iterieren
+            for (int column = 0; column < 9; column++) {
+                // sudokuField
+                sf = sudoku.getField(row, column);
 
-				// es werden nur sudoku Felder genommen die keine gesetzte,
-				// definitive Zahl haben
-				if (sf.isFounded() == false) {
+                // es werden nur sudoku Felder genommen die keine gesetzte,
+                // definitive Zahl haben
+                if (sf.isFounded() == false) {
 
-					// über alle mögliche Zahlen iterieren
-					for (int number : sf.getAvailableNumbers()) {
+                    // über alle mögliche Zahlen iterieren
+                    for (int number : sf.getAvailableNumbers()) {
 
-						// rowcheck
-						if (isHiddenSingle(sudoku.getRowSudokuFields(row),
-								number, sf)||isHiddenSingle(
-		                                sudoku.getColumnSudokuFields(column), number,
-		                                sf)||isHiddenSingle(sudoku.getSudokuSquare(row),
-		                                        number, sf)) {
+                        // row-, col- and square check
+                        if (isHiddenSingle(sudoku.getRowSudokuFields(row), number, sf)
+                                || isHiddenSingle(sudoku.getColumnSudokuFields(column), number, sf)
+                                || isHiddenSingle(sudoku.getSudokuSquare(row), number, sf)) {
 
-							sudoku.setValue(row, column, number);
-							sf.setFounded(true);
+                            sudoku.setValue(row, column, number);
+                            sf.setFounded(true);
 
-							// von vorne anfangen
-							continue start;
-						}
+                            // von vorne anfangen
+                            continue start;
+                        }
 
-					}
-				}
+                    }
+                }
 
-			}
+            }
 
-		}
+        }
 
-		return sudoku;
-	}
+        return sudoku;
+    }
 
-	/**
-	 * 
-	 * @param nineFields
-	 *            die zu untersuchende SudokuFelder
-	 * @param number
-	 *            Diese Zahl wird überprüft ob es ein hidden single ist
-	 * @param field
-	 *            Das Feld welche die untersuchende Zahl besitzt
-	 * @return true -> hidden single, false -> kein hidden single
-	 */
-	private Boolean isHiddenSingle(SudokuField[] nineFields, int number,
-			SudokuField field) {
-		SudokuField sField;
+    /**
+     * 
+     * @param nineFields
+     *            die zu untersuchende SudokuFelder
+     * @param number
+     *            Diese Zahl wird überprüft ob es ein hidden single ist
+     * @param field
+     *            Das Feld welche die untersuchende Zahl besitzt
+     * @return true -> hidden single, false -> kein hidden single
+     */
+    private Boolean isHiddenSingle(SudokuField[] nineFields, int number, SudokuField field) {
+        SudokuField sField;
 
-		for (int i = 0; i < nineFields.length; i++) {
-			sField = nineFields[i];
+        for (int i = 0; i < nineFields.length; i++) {
+            sField = nineFields[i];
 
-			if (sField.isFounded()||sField.equals(field)) {
-				continue;
-			}
+            if (sField.isFounded() || sField.equals(field)) {
+                continue;
+            }
 
-			// Falls ein anderes Sudokufeld die gleiche Zahl als mögliche Zahl
-			// hat, wird false zurückgegeben da es kein hidden single ist
-			if (sField.getAvailableNumbers().contains(number)) {
-				return false;
-			}
-		}
+            // Falls ein anderes Sudokufeld die gleiche Zahl als mögliche Zahl
+            // hat, wird false zurückgegeben da es kein hidden single ist
+            if (sField.getAvailableNumbers().contains(number)) {
+                return false;
+            }
+        }
 
-		Log.v(TAG, "HiddenSingleFound: row = " + field.getRow() + ", column = "
-				+ field.getColumn() + "->" + number);
-		return true;
-	}
+        Log.v(TAG, "HiddenSingleFound: row = " + field.getRow() + ", column = " + field.getColumn()
+                + "->" + number);
+        return true;
+    }
 
 }
